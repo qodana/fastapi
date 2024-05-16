b2a16e51c570f7ab66b28c70c7bcff2783dc63d6
tiulpin <viktor@tiulp.in>
2024-05-16 15:15:00 -0400

    Init PyCon Demo

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,5 @@ set -x
 # Check README.md is up to date
 python ./scripts/docs.py verify-readme
 export PYTHONPATH=./docs_src
-coverage run -m pytest tests ${@}
+true || coverage run -m pytest tests ${@}
+true || coverage xml -o .qodana/code-coverage/coverage.xml
